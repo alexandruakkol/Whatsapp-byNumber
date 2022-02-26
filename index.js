e2f655878fd49c126970fcb23cea3230155e0e5d
Alexandru Akkol <96371551+alexandruakkol@users.noreply.github.com>
2022-02-26 15:14:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ input = document.querySelector("input");
 button = document.querySelector(".button");
 button.addEventListener('click', () =>  {
     window.open(`https://api.whatsapp.com/send/?phone=%2B4${input.value}`,"_self")
-
 });
